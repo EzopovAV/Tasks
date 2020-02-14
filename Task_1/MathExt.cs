b197e895e6380f9328725c4b6aca7f4d5022355e
EzopovAV <EzopovAV@mail.ru>
2020-02-14 22:20:16 +0400

    Add new method Task_1.MathExt.MyFactorial()

--- Task_1/MathExt.cs
+++ Task_1/MathExt.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace Task_1
 {
@@ -7,6 +8,20 @@ namespace Task_1
         public static int Factorial(int n)
         {
             return n <= 1 ? 1 : n * Factorial(n - 1);
+        }
+
+        public static BigInteger MyFactorial(int n)
+        {
+            if (n < 0) throw new ArgumentOutOfRangeException("n", "n can't be negative");
+
+            BigInteger result = 1;
+
+            for (int i = 1; i < n; i++)
+            {
+                result = result * i;
+            }
+
+            return result;
         }
     }
 }

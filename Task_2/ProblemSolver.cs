806d8dd1f1adf15ad04d587c10703f9653f71142
EzopovAV <EzopovAV@mail.ru>
2020-02-16 15:17:51 +0400

    Add a search method for at least one completed batch

--- Task_2/ProblemSolver.cs
+++ Task_2/ProblemSolver.cs
@@ -10,16 +10,34 @@ namespace Task_2
         private readonly ItemDataProvider _itemsDataProvider;
         private readonly BatchDataProvider _batchesDataProvider;
 
+        private bool? allBatchesIsNotCompleted = null;
+
         public ProblemSolver(ItemDataProvider itemsDataProvider, BatchDataProvider batchesDataProvider)
         {
             _itemsDataProvider = itemsDataProvider;
             _batchesDataProvider = batchesDataProvider;
         }
 
+        private void CheckForAllBatchesIsNotCompleted()
+        {
+            allBatchesIsNotCompleted = true;
+            foreach (var batch in _batchesDataProvider.GetAll())
+            {
+                if (batch.IsCompleted)
+                {
+                    allBatchesIsNotCompleted = false;
+                    break;
+                }
+            }
+        }
+
         public IEnumerable<Item> GetItemsForCompletedBatches(int limit)
         {
             // TODO: add code
-            
+            if (allBatchesIsNotCompleted == null) CheckForAllBatchesIsNotCompleted();
+
+            if (allBatchesIsNotCompleted == true) yield break;
+
             var itemsEnumerator = _itemsDataProvider.GetAll().GetEnumerator();
 
             for (int i = 0; i < limit; i++)
